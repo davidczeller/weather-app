693ab67aaf466d9a3fc9105d349ba3d12bb9f8dd
David Czeller <david.czeller@cloudcode.hu>
2020-06-18 01:54:52 +0200

    daily forecast added. Background changes. Daily quote added.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,9 +7,9 @@ import Button from "@material-ui/core/Button";
 import moment from 'moment';
 
 import './Card.scss'
-import Sunny from '../static/images/sunny.gif'
-import Rainy from '../static/images/rain.gif'
-import Cloudy from '../static/images/cloud.gif'
+import Sunny from '../static/images/sunny.jpg'
+import Rainy from '../static/images/rain.jpg'
+import Cloudy from '../static/images/cloud.jpg'
 
 export default function Card() {
 
@@ -18,7 +18,9 @@ export default function Card() {
   const [city, setCity] = useState('Vienna')
   const [refresh, setRefresh] = useState(false)
   const [unit, setUnit] = useState('metric')
+  const [quote, setQuote] = useState('')
 
+  //current weather
   useEffect(() => {
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=bb6d0e601597f17b2c6b2e6bc419ac17&units=${unit}`)
     .then(res => res.json())
@@ -30,6 +32,7 @@ export default function Card() {
   }, [refresh]);
 
 
+  //hourly forecast
   useEffect(() => {
     fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=bb6d0e601597f17b2c6b2e6bc419ac17&units=${unit}`)
     .then(res => res.json())
@@ -37,28 +40,36 @@ export default function Card() {
     .catch(error => console.log(error));
   }, [refresh]);
 
-  //TODO
-  const sunset = currWeather && moment(currWeather.sys.sunset).format('hh:mm')
-  const sunrise = currWeather && moment(currWeather.sys.sunrise).format('hh:mm')
 
-  console.log(currWeather && {currWeather})
-  console.log(forecast && {forecast})
-  console.log({sunset, sunrise})
+  //quote
+  useEffect(() => {
+    fetch('http://quotes.rest/qod.json?category=inspire&maxlength=50')
+    .then(res => res.json())
+    .then(response => setQuote(response.contents.quotes[0]))
+    .catch(error => console.log(error));
+  }, [refresh]);
+
+
+  //TODO
+  // const sunset = currWeather && moment(currWeather.sys.sunset).format('hh:mm')
+  // const sunrise = currWeather && moment(currWeather.sys.sunrise).format('hh:mm')
+
+  // console.log(currWeather && {currWeather})
+  // console.log(forecast && {forecast})
+  // console.log({sunset, sunrise})
 
   const today = moment().format('dddd');
-  const now = moment()
 
 
   let icon = '-'
   let background = ''
-  const highArr = forecast && forecast.list.slice(0, 10).map(item => item.main.temp_max)
+  const highArr = forecast && forecast.list.slice(1, 11).map(item => item.main.temp_max)
   const highest = highArr && Math.max(...highArr)
-  const lowArr = forecast && forecast.list.slice(0, 10).map(item => item.main.temp_min)
+  const lowArr = forecast && forecast.list.slice(1, 11).map(item => item.main.temp_min)
   const lowest = lowArr && Math.min(...lowArr)
 
-  console.log({highArr, highest, lowArr, lowest, now})
 
-  const items = forecast && forecast.list.slice(0, 10).map(item => {
+  const items = forecast && forecast.list.slice(1, 11).map(item => {
     if (item.weather[0].description.includes('cloud')) {
       icon = <Cloud/>
       background = Cloudy
@@ -77,6 +88,44 @@ export default function Card() {
         <Typography>{`${item.dt_txt.charAt(11)}${item.dt_txt.charAt(12)}:00`}</Typography>
         {icon}
         <Typography>{`${Math.round(item.main.temp)}°`}</Typography>
+      </div>
+    )
+  })
+
+
+  const dailyForecastDayArr = forecast && forecast.list.filter((value, index) => index % 8 - 5 === 0)
+  const dailyForecastDay = dailyForecastDayArr && dailyForecastDayArr.map(item => item.main.temp)
+  const dailyForecastDay2 = dailyForecastDayArr && dailyForecastDayArr.map(item => item.weather[0].description)
+
+  const dailyForecastNightArr = forecast && forecast.list.filter((value, index) => index % 8 - 2 === 0)
+  const dailyForecastNight = dailyForecastNightArr && dailyForecastNightArr.map(item => item.main.temp)
+
+  let days = []
+  for (let i = 0; i < 5; i++) {
+    days.push(moment().add(i + 1, 'days').format('dddd'))
+  }
+
+
+  let bottomIcon = '-'
+  const day = days && days.map((item, index) => {
+    if (dailyForecastDay2 && dailyForecastDay2[index].includes('cloud')) {
+      bottomIcon = <Cloud fontSize="large"/>
+    }
+    if (dailyForecastDay2 && dailyForecastDay2[index].includes('rain')) {
+      bottomIcon = <Grain fontSize="large"/>
+    }
+    if ((dailyForecastDay2 && dailyForecastDay2[index].includes('sun'))
+      || (dailyForecastDay2 && dailyForecastDay2[index].includes('clear'))) {
+      bottomIcon = <WbSunny fontSize="large"/>
+    }
+    return (
+      <div key={index} className='forecastInner'>
+        <div>{item}</div>
+        <div>{bottomIcon}</div>
+        <div style={{display: 'flex', justifyContent: 'space-between'}}>
+          <div>{dailyForecastDay && Math.round(dailyForecastDay[index])}</div>
+          <div>{dailyForecastNight && Math.round(dailyForecastNight[index])}</div>
+        </div>
       </div>
     )
   })
@@ -160,6 +209,17 @@ export default function Card() {
           {items}
         </div>
         <div className='bottomContainer'>
+          <div className='forecast'>
+            {day}
+          </div>
+          <div className='quote'>
+            <div>
+              {quote.quote}
+            </div>
+            <div>
+              {quote.author}
+            </div>
+          </div>
         </div>
       </Paper>
     </div>
